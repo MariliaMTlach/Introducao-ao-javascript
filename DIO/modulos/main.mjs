d9da25a15a20d84075a4ab4f0fbf1a63ac93c10a
Mari <maritlach50@gmail.com>
2022-06-15 14:54:33 -0300

    adicionando o exercicio de modulos

---
+++ DIO/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Marília", 29));
+console.log(mostraCidade("Marília",  "Santo André"));
+console.log(mostraHobby("Marília", "Escutar Música"));
